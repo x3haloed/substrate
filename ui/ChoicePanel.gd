127376a594393d21804a8f79fbfb845e5f80fb4a
Chad Kirby <chadjkirby@icloud.com>
2025-11-03 08:36:32 -0800

    change choice panel to HFlow layout

--- ui/ChoicePanel.gd
+++ ui/ChoicePanel.gd
@@ -5,7 +5,7 @@ class_name ChoicePanel
 
 signal action_selected(verb: String, target: String)
 
-@onready var choice_container: GridContainer = $VBox/ScrollContainer/ChoiceContainer
+@onready var choice_container: HFlowContainer = $VBox/ScrollContainer/ChoiceContainer
 @onready var travel_dropdown: OptionButton = $VBox/TravelRow/TravelDropdown
 
 func clear_choices():
